2831199cdacc05284ce40d519ca4e630a2526d99
ZauberNerd <zaubernerd@zaubernerd.de>
2014-09-17 12:12:25 +0200

    added negative test cases for .is() method

--- test/fn.is.js
+++ test/fn.is.js
@@ -6,18 +6,22 @@
 
     describe('is function', function() {
 
-      var el;
-      var $el;
+      var el_div;
+      var el_span;
+      var $el_div;
+      var $el_span;
 
       beforeEach(function() {
-        el = document.createElement('div');
-        $el = $next(el);
+        el_span = document.createElement('span');
+        el_div = document.createElement('div');
+        $el_div = $next(el_div);
+        $el_span = $next(el_span);
         // HACK: phantomjs still uses an older webkit implementation..
         HTMLElement.prototype.matches = HTMLElement.prototype.webkitMatchesSelector;
       });
 
       afterEach(function() {
-        $el = null;
+        el_div = el_span = $el_div = $el_span = null;
         delete HTMLElement.prototype.matches;
       });
 
@@ -26,23 +30,43 @@
       });
 
       it('should match the element with a selector', function() {
-        var result = $el.is('div');
+        var result = $el_div.is('div');
         expect(result).toBe(true);
+      });
+
+      it('should not match the element with a different selector', function() {
+        var result = $el_div.is('span');
+        expect(result).toBe(false);
       });
 
       it('should match a HTMLElement', function() {
-        var result = $el.is(el);
+        var result = $el_div.is(el_div);
         expect(result).toBe(true);
+      });
+
+      it('should not match if given a different HTMLElement', function() {
+        var result = $el_div.is(el_span);
+        expect(result).toBe(false);
       });
 
       it('should match a jQueryNext instance', function() {
-        var result = $el.is($el);
+        var result = $el_div.is($el_div);
         expect(result).toBe(true);
       });
 
+      it('should not match a different jQueryNext instance', function() {
+        var result = $el_div.is($el_span);
+        expect(result).toBe(false);
+      });
+
       it('should match with a function', function() {
-        var result = $el.is(function(i, el) { return el.tagName === 'DIV'; });
+        var result = $el_div.is(function(i, el_div) { return el_div.tagName === 'DIV'; });
         expect(result).toBe(true);
+      });
+
+      it('should not match when the function returns false', function() {
+        var result = $el_div.is(function(i, el_div) { return false; });
+        expect(result).toBe(false);
       });
 
     });
