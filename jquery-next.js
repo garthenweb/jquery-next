7d93521b7fffedcad6d6117c17af7439aae65d2c
ZauberNerd <zaubernerd@zaubernerd.de>
2014-09-21 11:32:32 +0200

    use constructor to initialize new element set

--- jquery-next.js
+++ jquery-next.js
@@ -507,7 +507,7 @@
     },
 
     clone: function clone() {
-      return new this.init(this.elements.map(function(el) {
+      return this.constructor(this.elements.map(function(el) {
         return el.cloneNode(true);
       }));
     }
