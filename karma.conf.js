cfcd4ee88df1dab4849f18a3b1a89e769ad331c4
ZauberNerd <zaubernerd@zaubernerd.de>
2014-09-23 20:47:29 +0200

    run firefox tests with karma

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['PhantomJS'],
+    browsers: ['PhantomJS', 'Firefox'],
 
 
     // If browser does not capture in given timeout [ms], kill it
